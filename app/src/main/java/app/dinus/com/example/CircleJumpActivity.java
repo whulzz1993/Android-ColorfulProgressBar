ea13c6ab6108814d4ed4fdb875e3086448cbe65b
liuzhuangzhuang.das <liuzhuangzhuang.das@bytedance.com>
2021-09-09 13:34:38 +0800

    support android studio4.0

--- app/src/main/java/app/dinus/com/example/CircleJumpActivity.java
+++ app/src/main/java/app/dinus/com/example/CircleJumpActivity.java
@@ -3,7 +3,8 @@ package app.dinus.com.example;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CircleJumpActivity extends AppCompatActivity {
     public static void startActivity(Context context) {
--- app/src/main/java/app/dinus/com/example/CircleRotateActivity.java
+++ app/src/main/java/app/dinus/com/example/CircleRotateActivity.java
@@ -2,17 +2,9 @@ package app.dinus.com.example;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-import app.dinus.com.loadingdrawable.DensityUtil;
-import app.dinus.com.loadingdrawable.LoadingView;
-import app.dinus.com.loadingdrawable.render.LoadingRenderer;
-import app.dinus.com.loadingdrawable.render.circle.rotate.GearLoadingRenderer;
-import app.dinus.com.loadingdrawable.render.circle.rotate.LevelLoadingRenderer;
-import app.dinus.com.loadingdrawable.render.circle.rotate.MaterialLoadingRenderer;
-import app.dinus.com.loadingdrawable.render.circle.rotate.WhorlLoadingRenderer;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CircleRotateActivity extends AppCompatActivity {
     public static void startActivity(Context context) {
